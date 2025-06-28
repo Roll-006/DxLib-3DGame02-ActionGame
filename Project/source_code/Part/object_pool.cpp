2820ac0124655df39a65b5a23381e1e64933be96
Roll-006 <setokazuma0617@gmail.com>
2025-06-28 19:43:43 +0900

    オブジェクトプール用ファイルを追加

---
+++ Project/source_code/Part/object_pool.cpp
@@ -0,0 +1,146 @@
+﻿#include "animator.hpp"
+
+Animator::Animator(const std::shared_ptr<Modeler> modeler) :
+	m_prev_anim_play_rate		(0.0f),
+	m_blend_rate				(1.0f),
+	m_is_first_frame_change_anim(false),
+	m_is_attached				(false),
+	m_modeler					(modeler)
+
+{
+	m_time_kind_data[TimeKind::kPrev] = m_time_kind_data[TimeKind::kCurrent] = AnimTimeKindData();
+}
+
+Animator::~Animator()
+{
+	for (auto& data : m_anim_data)
+	{
+		MV1DeleteModel(data.second.anim_handle);
+	}
+}
+
+void Animator::Init()
+{
+	DetachAnim(TimeKind::kPrev);
+	DetachAnim(TimeKind::kCurrent);
+}
+
+void Animator::Update()
+{
+	m_is_first_frame_change_anim = false;
+
+	BlendAnim();
+	PlayAnim();
+}
+
+void Animator::AddAnimHandle(const int kind, const std::string& file_path, const int index, const std::string& tag, const float play_speed, const bool is_loop)
+{
+	// 上書き不可
+	if (m_anim_data.count(kind)) { return; }
+
+	int handle = HandleKeeper::GetInstance()->LoadHandle(HandleKind::kAnim, file_path);
+	if (handle != -1)
+	{
+		m_anim_data[kind] = AnimKindData(handle, index, tag, play_speed, is_loop);
+	}
+}
+
+void Animator::AddAnimHandle(const int kind, const int anim_handle, const int index, const std::string& tag, const float play_speed, const bool is_loop)
+{
+	// 上書き不可
+	if (m_anim_data.count(kind)) { return; }
+
+	if (anim_handle != -1)
+	{
+		m_anim_data[kind] = AnimKindData(anim_handle, index, tag, play_speed, is_loop);
+	}
+}
+
+void Animator::AttachAnim(const int next_kind)
+{
+	// 現在のアニメーションと同じであった場合はアタッチを許可しない
+	// ブレンドが完了していない場合はアタッチを許可しない
+	if (m_time_kind_data.at(TimeKind::kCurrent).kind == next_kind || m_blend_rate != 1.0f)
+	{
+		m_is_attached = false;
+		return;
+	}
+
+	DetachAnim(TimeKind::kPrev);
+
+	m_is_attached					= true;
+	m_is_first_frame_change_anim	= true;
+
+	// データをシフト(Current ➡ Prev, Next ➡ Current)
+	m_time_kind_data.at(TimeKind::kPrev)					= m_time_kind_data.at(TimeKind::kCurrent);
+	m_time_kind_data.at(TimeKind::kCurrent).attach_index	= MV1AttachAnim(m_modeler->GetModelHandle(), m_anim_data.at(next_kind).index, m_anim_data.at(next_kind).anim_handle, TRUE);
+	m_time_kind_data.at(TimeKind::kCurrent).kind			= next_kind;
+	SetPlayStartTime();
+
+	// 前回のアニメーションが存在しない場合は、ブレンド済み(ブレンド率100%)とする
+	m_blend_rate = m_time_kind_data.at(TimeKind::kPrev).attach_index > -1 ? 0.0f : 1.0f;
+}
+
+void Animator::DetachAnim(const TimeKind time_kind)
+{
+	if (m_time_kind_data.at(time_kind).attach_index > -1)
+	{
+		MV1DetachAnim(m_modeler->GetModelHandle(), m_time_kind_data.at(time_kind).attach_index);
+		m_time_kind_data.at(time_kind).attach_index = -1;
+	}
+}
+
+void Animator::SetPlayStartTime()
+{
+	if (m_anim_data.count(m_time_kind_data.at(TimeKind::kPrev).kind))
+	{
+		const std::string prev_tag	  = m_anim_data.at(m_time_kind_data.at(TimeKind::kPrev).kind).tag;
+		const std::string current_tag = m_anim_data.at(m_time_kind_data.at(TimeKind::kCurrent).kind).tag;
+
+		// 同類アニメーションであった場合は再生率を引き継ぐ
+		if (prev_tag == current_tag)
+		{
+			const float current_total_t = MV1GetAttachAnimTotalTime(m_modeler->GetModelHandle(), m_time_kind_data.at(TimeKind::kCurrent).attach_index);
+			const float prev_total_t	= MV1GetAttachAnimTotalTime(m_modeler->GetModelHandle(), m_time_kind_data.at(TimeKind::kPrev).attach_index);
+
+			m_prev_anim_play_rate = m_time_kind_data.at(TimeKind::kPrev).play_timer / prev_total_t;
+			m_time_kind_data.at(TimeKind::kCurrent).play_timer = current_total_t * m_prev_anim_play_rate;
+
+			return;
+		}
+	}
+
+	m_time_kind_data.at(TimeKind::kCurrent).play_timer = 0.0f;
+}
+
+void Animator::PlayAnim()
+{
+	for (auto& [state_t, data] : m_time_kind_data)
+	{
+		// アニメーションが有効であった場合のみ再生
+		if (data.attach_index > -1)
+		{
+			const float total_t = MV1GetAttachAnimTotalTime(m_modeler->GetModelHandle(), data.attach_index);
+			const float blend_r = state_t == TimeKind::kCurrent ? m_blend_rate : 1.0f - m_blend_rate;
+
+			float play_speed = m_anim_data.at(data.kind).play_speed * FPS::GetDeltaTime();
+			math::IncreaseLoop(data.play_timer, play_speed, total_t, m_anim_data.at(data.kind).is_loop);
+
+			// 再生位置・ブレンド率を適用
+			MV1SetAttachAnimTime	 (m_modeler->GetModelHandle(), data.attach_index, data.play_timer);
+			MV1SetAttachAnimBlendRate(m_modeler->GetModelHandle(), data.attach_index, blend_r);
+		}
+	}
+}
+
+void Animator::BlendAnim()
+{
+	// ブレンド率100%まで増加させる
+	math::Increase(m_blend_rate, kBlendSpeed * FPS::GetDeltaTime(), 1.0f);
+  
+	// ブレンドが完了した場合、PravAnimは不要なためデタッチする
+	if (m_blend_rate == 1.0f)
+	{
+		DetachAnim(TimeKind::kPrev);
+	}
+}
---
+++ Project/source_code/Part/object_pool.hpp
@@ -0,0 +1,80 @@
+#pragma once
+#include <unordered_map>
+
+#include "../Manager/handle_keeper.hpp"
+#include "../Data/Tag/animation_tag.hpp"
+#include "../Data/animator_data.hpp"
+#include "../Concept/common_concepts.hpp"
+#include "../FPS/fps.hpp"
+
+#include "modeler.hpp"
+
+class Animator final
+{
+public:
+	Animator(const std::shared_ptr<Modeler> modeler);
+	~Animator();
+
+	void Init();
+	void Update();
+
+	/// @brief アニメーションをアタッチする
+	/// @brief デタッチ処理は自動的に実行
+	void AttachAnim(const int next_kind);
+
+	void AddAnimHandle(const int kind, const std::string& file_path, const int index, const std::string& tag, const float play_speed, const bool is_loop);
+	void AddAnimHandle(const int kind, const int anim_handle,		 const int index, const std::string& tag, const float play_speed, const bool is_loop);
+
+	/// @brief 直近でアタッチを試みたアニメーションがアタッチされたかを判定
+	[[nodiscard]] bool IsAttached() const { m_is_attached; }
+
+	/// @brief 再生されているアニメーションの種類を取得
+	[[nodiscard]] int GetAnimKind(const TimeKind time_kind) const { m_time_kind_data.at(time_kind).kind; }
+
+	/// @brief ブレンド率を取得する
+	[[nodiscard]] float GetBlendRate()const { m_blend_rate; }
+
+private:
+	void DetachAnim(const TimeKind time_kind);
+
+	/// @brief 再生開始地点を設定
+	void SetPlayStartTime();
+
+	void PlayAnim();
+	void BlendAnim();
+
+private:
+	static constexpr float kBlendSpeed = 3.0f;
+
+	std::unordered_map<int, AnimKindData>			m_anim_data;
+	std::unordered_map<TimeKind, AnimTimeKindData>	m_time_kind_data;
+
+	float m_prev_anim_play_rate;			// 前回のアニメーションの再生率
+	float m_blend_rate;
+
+	bool  m_is_first_frame_change_anim;		// アニメーションが変更されて最初のフレームであるかを判定
+	bool  m_is_attached;					// 直近でアタッチを試みたアニメーションがアタッチされたかを判定
+
+	std::shared_ptr<Modeler> m_modeler;
+};
+
+
+
+//
+//std::unordered_map<int, int> map;
+//
+//map[key] = value;
+//
+//int a = map[key];		// もしこのkeyがまだなかったら自動的にkeyが生成される
+//int b = map.at(key);	// エラー
+//
+//map.erase(key);
+//
+//if (map.count(key))
+//{
+//	// keyを作った
+//}
+//else
+//{
+//	// 作ってない
+//}
