2db9739df5241de7ff539356bbfeb56fe35cc8db
水野亮汰 <setokazuma0617@gmail.com>
2025-08-26 14:18:05 +0900

    IEnemyRoleを作成中

---
+++ Project/SourceCode/Data/Kind/enemy_role_kind.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+/// @brief “G‚Ì–ğŠ„
+enum class EnemyRoleKind
+{
+	kAttaccker,		// ’ÇÕ•”‘à
+	kFlanker,		// ‰ñ‚è‚İ•”‘à
+	kZoneDefender,	// ‘Ò‚¿•š‚¹•”‘à
+};
---
+++ Project/SourceCode/Interface/i_enemy_role.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <memory>
+#include "../Interface/i_enemy.hpp"
+#include "../Data/Kind/enemy_role_kind.hpp"
+
+class IEnemyRole abstract
+{
+public:
+	virtual ~IEnemyRole() = default;
+
+	virtual void CalcDistanceToTarget() abstract;
+
+	[[nodiscard]] virtual EnemyRoleKind GetRoleKind() const abstract;
+	[[nodiscard]] virtual std::shared_ptr<IEnemyRole> ChangeRole(std::shared_ptr<IEnemy> enemy) abstract;
+};
