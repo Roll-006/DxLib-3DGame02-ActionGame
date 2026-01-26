effc29c934a25c11c9fe276fd95834469f1ff159
Roll-006 <setokazuma0617@gmail.com>
2026-01-26 16:23:25 +0900

    リリース時にはフルスクリーンが初期状態の設定を復帰

--- Project/SourceCode/Window/window.cpp
+++ Project/SourceCode/Window/window.cpp
@@ -2,12 +2,13 @@
 
 Window::Window():
 	m_is_full_screen		(true),
-	m_change_window_contains(m_is_full_screen ? 1 : 0)
+	m_change_window_count	(m_is_full_screen ? 1 : 0)
 {
-	//#ifdef _DEBUG
-	m_is_full_screen = false;
-	m_change_window_contains = 0;
-	//#endif
+	#ifdef _DEBUG
+	m_is_full_screen		= false;
+	m_change_window_count	
+		= 0;
+	#endif
 
 	SetWindowMode(m_is_full_screen);
 }
@@ -33,15 +34,15 @@ void Window::SetWindowMode()
 
 	if (is_change)
 	{
-		++m_change_window_contains;
-		m_is_full_screen = m_change_window_contains % 2 == 0;
+		++m_change_window_count;
+		m_is_full_screen = m_change_window_count % 2 == 0;
 		ChangeWindowMode(m_is_full_screen ? FALSE : TRUE);
 	}
 }
 
 void Window::SetWindowMode(const bool is_full_screen)
 {
-	++m_change_window_contains;
+	++m_change_window_count;
 	m_is_full_screen = is_full_screen;
 	ChangeWindowMode(m_is_full_screen ? FALSE : TRUE);
 }
