f969db0ac6db0e0726e2c298b0ead973645595a6
Roll <setokazuma0617@gmail.com>
2026-02-05 09:00:59 +0900

    画像追加

--- Project/SourceCode/Part/button_graphic_getter.cpp
+++ Project/SourceCode/Part/button_graphic_getter.cpp
@@ -1,13 +1,16 @@
-#include "button_graphic_getter.hpp"
+﻿#include "button_graphic_getter.hpp"
 
 ButtonGraphicGetter::ButtonGraphicGetter()
 {
-	m_input_graphics[InputCode(InputKind::kKey,			static_cast<int>(KEY_INPUT_F))]					= std::make_shared<Graphicer>(UIGraphicPath.KEY_F);
-	m_input_graphics[InputCode(InputKind::kKey,			static_cast<int>(KEY_INPUT_SPACE))]				= std::make_shared<Graphicer>(UIGraphicPath.KEY_SPACE);
+	m_input_graphics[InputCode(InputKind::kKey,			static_cast<int>(KEY_INPUT_R))]					= std::make_shared<Graphicer>("Data/Graphic/UI/Button/Key/R.png");
+	m_input_graphics[InputCode(InputKind::kKey,			static_cast<int>(KEY_INPUT_F))]					= std::make_shared<Graphicer>("Data/Graphic/UI/Button/Key/F.png");
 	m_input_graphics[InputCode(InputKind::kKey,			static_cast<int>(KEY_INPUT_W))]					= std::make_shared<Graphicer>("Data/Graphic/UI/Button/Key/W.png");
 	m_input_graphics[InputCode(InputKind::kKey,			static_cast<int>(KEY_INPUT_A))]					= std::make_shared<Graphicer>("Data/Graphic/UI/Button/Key/A.png");
 	m_input_graphics[InputCode(InputKind::kKey,			static_cast<int>(KEY_INPUT_S))]					= std::make_shared<Graphicer>("Data/Graphic/UI/Button/Key/S.png");
 	m_input_graphics[InputCode(InputKind::kKey,			static_cast<int>(KEY_INPUT_D))]					= std::make_shared<Graphicer>("Data/Graphic/UI/Button/Key/D.png");
+	m_input_graphics[InputCode(InputKind::kKey,			static_cast<int>(KEY_INPUT_SPACE))]				= std::make_shared<Graphicer>("Data/Graphic/UI/Button/Key/SPACE.png");
+	m_input_graphics[InputCode(InputKind::kKey,			static_cast<int>(KEY_INPUT_ESCAPE))]			= std::make_shared<Graphicer>("Data/Graphic/UI/Button/Key/ESCAPE.png");
+	m_input_graphics[InputCode(InputKind::kKey,			static_cast<int>(KEY_INPUT_LSHIFT))]			= std::make_shared<Graphicer>("Data/Graphic/UI/Button/Key/SHIFT_Left.png");
 	
 	m_input_graphics[InputCode(InputKind::kMouseButton,	static_cast<int>(mouse::ButtonKind::kLeft))]	= std::make_shared<Graphicer>(UIGraphicPath.MOUSE_LEFT);
 	m_input_graphics[InputCode(InputKind::kMouseButton,	static_cast<int>(mouse::ButtonKind::kMiddle))]	= std::make_shared<Graphicer>(UIGraphicPath.MOUSE_MIDDLE);
@@ -18,8 +21,10 @@ ButtonGraphicGetter::ButtonGraphicGetter()
 	m_input_graphics[InputCode(InputKind::kMouseSlide,	static_cast<int>(mouse::SlideDirKind::kDown))]	= std::make_shared<Graphicer>("Data/Graphic/UI/Button/Mouse/none.png");
 	m_input_graphics[InputCode(InputKind::kMouseSlide,	static_cast<int>(mouse::SlideDirKind::kRight))]	= std::make_shared<Graphicer>("Data/Graphic/UI/Button/Mouse/none.png");
 	
-	m_input_graphics[InputCode(InputKind::kPadButton,	static_cast<int>(pad::ButtonKind::kA))]			= std::make_shared<Graphicer>(UIGraphicPath.PAD_A);
-	m_input_graphics[InputCode(InputKind::kPadButton,	static_cast<int>(pad::ButtonKind::kB))]			= std::make_shared<Graphicer>(UIGraphicPath.PAD_B);
+	m_input_graphics[InputCode(InputKind::kPadButton,	static_cast<int>(pad::ButtonKind::kA))]			= std::make_shared<Graphicer>("Data/Graphic/UI/Button/Pad/A.png");
+	m_input_graphics[InputCode(InputKind::kPadButton,	static_cast<int>(pad::ButtonKind::kB))]			= std::make_shared<Graphicer>("Data/Graphic/UI/Button/Pad/B.png");
+	m_input_graphics[InputCode(InputKind::kPadButton,	static_cast<int>(pad::ButtonKind::kX))]			= std::make_shared<Graphicer>("Data/Graphic/UI/Button/Pad/X.png");
+	m_input_graphics[InputCode(InputKind::kPadButton,	static_cast<int>(pad::ButtonKind::kY))]			= std::make_shared<Graphicer>("Data/Graphic/UI/Button/Pad/Y.png");
 
 	m_input_graphics[InputCode(InputKind::kPadTrigger,	static_cast<int>(pad::TriggerKind::kRT))]		= std::make_shared<Graphicer>("Data/Graphic/UI/Button/Pad/RT.png");
 	m_input_graphics[InputCode(InputKind::kPadTrigger,	static_cast<int>(pad::TriggerKind::kLT))]		= std::make_shared<Graphicer>("Data/Graphic/UI/Button/Pad/LT.png");
